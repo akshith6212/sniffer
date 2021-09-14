a5f60a9a3c559e174ce64a32a5ebbb4108dc6f2f
akshith6212 <akshith6212@gmail.com>
2021-09-14 12:02:16 +0530

    writes only udp packets

---
+++ sniffer.py
@@ -0,0 +1,48 @@
+import socket
+from general import *
+from networking.ethernet import Ethernet
+from networking.ipv4 import IPv4
+from networking.udp import UDP
+from networking.pcap import Pcap
+
+TAB_1 = '\t - '
+TAB_2 = '\t\t - '
+TAB_3 = '\t\t\t - '
+TAB_4 = '\t\t\t\t - '
+
+DATA_TAB_1 = '\t   '
+DATA_TAB_2 = '\t\t   '
+DATA_TAB_3 = '\t\t\t   '
+DATA_TAB_4 = '\t\t\t\t   '
+
+def main():
+    pcap = Pcap('capture.pcap')
+    conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+
+    while True:
+        raw_data, addr = conn.recvfrom(65535)
+        eth = Ethernet(raw_data)
+
+        print('\nEthernet Frame:')
+        print(TAB_1 + 'Destination: {}, Source: {}, Protocol: {}'.format(eth.dest_mac, eth.src_mac, eth.proto))
+
+        # IPv4
+        if eth.proto == 8:
+            ipv4 = IPv4(eth.data)
+            print(TAB_1 + 'IPv4 Packet:')
+            print(TAB_2 + 'Version: {}, Header Length: {}, TTL: {},'.format(ipv4.version, ipv4.header_length, ipv4.ttl))
+            print(TAB_2 + 'Protocol: {}, Source: {}, Target: {}'.format(ipv4.proto, ipv4.src, ipv4.target))
+
+            # UDP
+            if ipv4.proto == 17:
+                udp = UDP(ipv4.data)
+                print(TAB_1 + 'UDP Segment:')
+                print(TAB_2 + 'Source Port: {}, Destination Port: {}, Length: {}'.format(udp.src_port, udp.dest_port, udp.size))
+                
+                # only incoming udp packets needed
+                if ipv4.target == '192.168.43.6':
+                    pcap.write(raw_data)
+
+    pcap.close()
+
+main()
